30b5c158ef078828879a77ef96fed131e98623ac
Ignat Simonenko <simonenkoignat@gmail.com>
2018-10-17 09:48:18 +0300

    Change size of basic list, add tests for addAll method

--- src/main/java/edu/khai/simonenko/collections/adding/AddElementsToListWithoutInitialCapacityAndWith.java
+++ src/main/java/edu/khai/simonenko/collections/adding/AddElementsToListWithoutInitialCapacityAndWith.java
@@ -8,10 +8,10 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @State(Scope.Benchmark)
-@Warmup(iterations = 5, time = 25, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 10, time = 25, timeUnit = TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 20, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 30, time = 20, timeUnit = TimeUnit.MILLISECONDS)
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @CompilerControl(CompilerControl.Mode.DONT_INLINE)
 public class AddElementsToListWithoutInitialCapacityAndWith {
 
@@ -19,30 +19,44 @@ public class AddElementsToListWithoutInitialCapacityAndWith {
 
     @Setup
     public void init() {
-        for (int i = 0; i < 1_000_000; ++i) {
+        for (int i = 0; i < 100; ++i) {
             LIST.add(RandomStringUtils.randomAlphanumeric(5));
         }
     }
 
     @Benchmark
-    public void testAddingElementToListWithoutInitialCapacity() {
+    public void testAddingElementsToListOneByOneWithoutInitialCapacity() {
         List<String> result = new ArrayList<>();
-        for (var string : LIST) {
-            result.add(string);
+        for (var element : LIST) {
+            result.add(element);
         }
     }
 
     @Benchmark
-    public void testAddingElementToListWithInitialCapacity() {
+    public void testAddingAllElementsToListWithoutInitialCapacity() {
+        List<String> result = new ArrayList<>();
+        result.addAll(LIST);
+    }
+
+    @Benchmark
+    public void testAddingElementsToListOneByOneWithInitialCapacity() {
         List<String> result = new ArrayList<>(LIST.size());
-        for (var string : LIST) {
-            result.add(string);
+        for (var element : LIST) {
+            result.add(element);
         }
     }
 
+    @Benchmark
+    public void testAddingAllElementsToListWithInitialCapacity() {
+        List<String> result = new ArrayList<>(LIST.size());
+        result.addAll(LIST);
+    }
+
     /*
-     * Benchmark                                                                                     Mode  Cnt   Score   Error  Units
-     * AddElementsToListWithoutInitialCapacityAndWith.testAddingElementToListWithInitialCapacity     avgt   50  12.715 ± 1.574  ms/op
-     * AddElementsToListWithoutInitialCapacityAndWith.testAddingElementToListWithoutInitialCapacity  avgt   50  13.205 ± 0.495  ms/op
+     * Benchmark                                                                                              Mode  Cnt    Score    Error  Units
+     * AddElementsToListWithoutInitialCapacityAndWith.testAddingAllElementsToListWithInitialCapacity          avgt  150  165.141 ± 24.709  ns/op
+     * AddElementsToListWithoutInitialCapacityAndWith.testAddingAllElementsToListWithoutInitialCapacity       avgt  150  162.037 ± 23.617  ns/op
+     * AddElementsToListWithoutInitialCapacityAndWith.testAddingElementsToListOneByOneWithInitialCapacity     avgt  150  429.116 ± 15.566  ns/op
+     * AddElementsToListWithoutInitialCapacityAndWith.testAddingElementsToListOneByOneWithoutInitialCapacity  avgt  150  597.379 ± 53.492  ns/op
      */
 }
